5966be19fd23ac11267acabf9d0bdc999872fcd8
marc.salas <marc.salas@62489485-163e-0410-951f-2d2251ec6b19>
2007-11-04 10:37:25 +0000

    Creacio del projecte eSandwich del grup SAMOSA

---
+++ eSandwich/src/samosa/Main.java
@@ -0,0 +1,29 @@
+/*
+ * Main.java
+ *
+ * Created on 4. marraskuuta 2007, 12:30
+ *
+ * To change this template, choose Tools | Template Manager
+ * and open the template in the editor.
+ */
+
+package samosa;
+
+/**
+ *
+ * @author Marc
+ */
+public class Main {
+    
+    /** Creates a new instance of Main */
+    public Main() {
+    }
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
